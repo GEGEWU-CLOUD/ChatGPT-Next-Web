d882d08f77de6f380c388738dfd0e9c974738364
GEGEWU-CLOUD <100192770+GEGEWU-CLOUD@users.noreply.github.com>
2024-12-26 09:53:50 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -156,7 +156,6 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
           Chatzpt.
-          Build your own AI assistant.
         </div>
         <div className={styles["sidebar-sub-title"]}>
           欢迎使用来自ZzNnWn的ai助手.
